7204ac6e66dc59f23516a8ee4d235bbc727525ff
Waleedanwar01 <waleeddogare@gmail.com>
2025-09-01 18:59:55 +0500

    new cors email

--- backend/views.py
+++ backend/views.py
@@ -3,6 +3,8 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 from django.conf import settings
+
+
 @csrf_exempt
 def contactUs(request):
     if request.method == "POST":
@@ -11,11 +13,11 @@ def contactUs(request):
             name = data.get("name")
             email = data.get("email")
             message = data.get("message")
-            
+
             # Check if any field is missing
             if not name or not email or not message:
                 return JsonResponse({"error": "All fields are required!"}, status=400)
-            
+
             # Send email
             send_mail(
                 subject=f"New Contact Message from {name}",
@@ -24,9 +26,9 @@ def contactUs(request):
                 recipient_list=["waleeddogare@gmail.com"],
                 fail_silently=False,
             )
-            
+
             return JsonResponse({"message": "Message sent successfully!"})
-        
+
         except Exception as e:
             print("Error sending email:", e)
             return JsonResponse({"error": "Failed to send email"}, status=500)
